1d7aaed1c4db27cec49930c0c1f8a20fd59b8408
FarihaTahsin <83426539+FarihaTahsin@users.noreply.github.com>
2021-04-30 02:46:09 +0600

    Add files via upload

---
+++ view/feature4.php
@@ -0,0 +1,34 @@
+<?php 
+session_start();
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Feature 4</title>
+	 <!--link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"-->
+	 <link rel="stylesheet" href="../view/css/stylee.css">
+
+</head>
+<body>
+	<div class="mrg">
+		<div class="firstbg">
+			<div class="nav">
+				<ul>
+	                <li><a href="../controller/placeorder.php">Place Order</a></li>
+	                <li><a href="../controller/vieworder.php">View Order Status</a></li>
+	                <li><a href="../controller/menu.php">Menu</a></li>
+	                <li><a href="../controller/index.php">Log out</a></li>
+	                <li><a href="../controller/admin.php">Admin</a></li>
+	            </ul>
+	        </div>
+	    
+	
+	<h1 class="mrg">Boma Burgers</h1>
+		<p><span style="color: #315355f ">Bringing in the solution to all your burger cravings</span></p>
+	    
+	</div>
+</div>
+	
+
+</body>
+</html>
---
+++ view/header.php
@@ -0,0 +1,38 @@
+
+<head>
+<title> Boma Burger </title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+<style type = "text/css">
+
+  .brand{
+      background: #ba4a00 !important;
+      background: url("../view/images/burgerr.jpg");
+      background-size: cover ;
+
+  }
+  .brand-text{
+      color: yellow !important;
+  }
+ 
+  .form{
+      max-width : 460px;
+      margin: 20px auto;
+      padding: 20px;
+  }
+  footer{text-align: center;} 
+
+</style>
+
+
+
+</head>
+
+<body class = "grey lighten-4">
+<nav class = "grey z-depth-0">
+ <div class = "cointainer">
+    <ul id = "nav-mobile" class ="right hide-on-small-and-down">
+       <li><a href = "../view/feature4.php" class ="btn brand z-depth-0"> Home </a><li>
+       <li><a href = "../controller/vieworder2.php" class ="btn brand z-depth-0"> View Orders</a><li>
+</div>
+</nav>
